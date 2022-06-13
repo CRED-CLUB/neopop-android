85aa5c62daf9c914cf2cd60e123aa068fa0fb76e
Bhuvanesh <bhuvaneshkumar1729@gmail.com>
2022-06-13 15:24:29 +0530

    feat(pop): added licensing

--- neopop/src/main/java/club/cred/neopop/PopFrameLayout.kt
+++ neopop/src/main/java/club/cred/neopop/PopFrameLayout.kt
@@ -1,3 +1,15 @@
+/*
+ *
+ *  * Copyright 2022 Dreamplug Technologies Private Limited
+ *  * Licensed under the Apache License, Version 2.0 (the “License”);
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *     http://www.apache.org/licenses/LICENSE-2.0
+ *  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an “AS IS” BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and limitations under the License.
+ *
+ */
+
 package club.cred.neopop
 
 import android.animation.ValueAnimator
@@ -17,13 +29,13 @@ import club.cred.neopop.common.dynamicAttr
 import club.cred.neopop.common.provideSafeHapticFeedback
 import club.cred.neopop.common.withAttrs
 import club.cred.neopop.popButton.Colors
-import club.cred.neopop.popButton.SurfaceStrokeColorData
-import club.cred.neopop.popButton.SurfaceStrokeColors
 import club.cred.neopop.popButton.NeoPopGeometry
 import club.cred.neopop.popButton.NeoPopHelper
 import club.cred.neopop.popButton.NeoPopHelper.calculateButtonStyles
 import club.cred.neopop.popButton.PopDrawable
 import club.cred.neopop.popButton.PopFrameLayoutStyle
+import club.cred.neopop.popButton.SurfaceStrokeColorData
+import club.cred.neopop.popButton.SurfaceStrokeColors
 import com.dreamplug.neopop.R
 import kotlin.math.roundToInt
 import kotlin.properties.ReadWriteProperty
